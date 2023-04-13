71d1ce472b0f2643d738b5f75c54f1ed21430e7d
Kao <husb@live.hk>
2023-04-14 02:37:12 +0800

    - fix nav graph

--- app/src/main/java/com/example/project_g01/adapter/ItineraryAdapter.kt
+++ app/src/main/java/com/example/project_g01/adapter/ItineraryAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.project_g01.adapter
 
-class ItineraryAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.project_g01.R
+import com.example.project_g01.models.Itinerary
+
+class ItineraryAdapter(context: Context, itineraryList: List<Itinerary>)
+    : ArrayAdapter<Itinerary> (context, 0, itineraryList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.list_item_itinerary, parent, false)
+
+        val itinerary = getItem(position)
+
+        itinerary?.let {
+            val tvParkName: TextView = view.findViewById(R.id.tvParkName)
+            val tvParkAddress: TextView = view.findViewById(R.id.tvParkAddress)
+            val tvTripDate: TextView = view.findViewById(R.id.tvTripDate)
+            
+            tvParkName.text = "Substitute DB values..."
+            tvParkAddress.text = "Substitute DB values..."
+            tvTripDate.text = "Substitute DB values..."
+        }
+
+        return view
+    }
 }
